f7883b140466535280b1bc6f7c7693e221162c40
Luciano Handal <lhandal98@gmail.com>
2025-08-16 18:39:50 -0400

    Initial commit

--- static/casa.js
+++ static/casa.js
@@ -1,13 +1,13 @@
 const body = $('body')[0];
 const navbar = $("#navbar")[0];
 const nav_items = $('.nav-item');
-const hello_h1 = $("#hello_world")[0];
+// const hello_h1 = $("#hello_world")[0];
 const url = document.URL.split("?")[0];
-const hello_msg = "Hello World!";
-const hello_msg_html = "Hello World!<span id='new_line'>\\n</span>";
-const projects = $('.project_div');
-const contact_form = $("#contact_form")[0];
-const query = url.split('?',2)[1];
+// const hello_msg = "Hello World!";
+// const hello_msg_html = "Hello World!<span id='new_line'>\\n</span>";
+// const projects = $('.project_div');
+// const contact_form = $("#contact_form")[0];
+// const query = url.split('?',2)[1];
 var current_section = 'hello';
 var current_color = '#222831';
 var sectionOffsets;
@@ -51,16 +51,16 @@ function colorCoordinate() {
   $(body).css('background-color', current_color)
 }
 
-function scrollNavbar(){
-  let scrollPosition = $(window).scrollTop() + navbar.offsetHeight;
-  for (var i = 0; i < sectionOffsets.length; i++) {
-    if (scrollPosition < sectionOffsets[i].offset) {
-      showPosition(i - 1)
-      colorCoordinate();
-      break
-    }
-  }
-}
+// function scrollNavbar(){
+//   let scrollPosition = $(window).scrollTop() + navbar.offsetHeight;
+//   for (var i = 0; i < sectionOffsets.length; i++) {
+//     if (scrollPosition < sectionOffsets[i].offset) {
+//       showPosition(i - 1)
+//       colorCoordinate();
+//       break
+//     }
+//   }
+// }
 
 function goToSection(section_id){
   if (sectNum[section_id] == null) {
@@ -73,22 +73,22 @@ function goToSection(section_id){
     function(){window.scrollTo(0, location);});
 }
 
-function typeChar(i) {
-  hello_h1.innerText = hello_msg.substring(0, i) + '|';
-}
+// function typeChar(i) {
+//   hello_h1.innerText = hello_msg.substring(0, i) + '|';
+// }
 
-function typeHello() {
-  const rate = 100;
-  var time;
-  let msg_length = hello_msg.length;
-  for (let i = 0; i < msg_length; i++) {
-    time = i * rate;
-    setTimeout(function(){ typeChar(i); }, time);
-  }
-  time += rate;
-  setTimeout(function(){hello_h1.innerHTML = hello_msg_html;}, time);
-  time *= 2;
-}
+// function typeHello() {
+//   const rate = 100;
+//   var time;
+//   let msg_length = hello_msg.length;
+//   for (let i = 0; i < msg_length; i++) {
+//     time = i * rate;
+//     setTimeout(function(){ typeChar(i); }, time);
+//   }
+//   time += rate;
+//   setTimeout(function(){hello_h1.innerHTML = hello_msg_html;}, time);
+//   time *= 2;
+// }
 
 function copyToClipboard(str) {
   const tempInput = document.createElement("input");
@@ -120,52 +120,52 @@ function toggleIcon() {
   $('#nav_icon').toggleClass('fa-times');
 }
 
-var form = document.getElementById("contact_form");
+// var form = document.getElementById("contact_form");
+//
+// async function handleSubmit(event) {
+//   event.preventDefault();
+//   var status = document.getElementById("contact_status");
+//   var data = new FormData(event.target);
+//   fetch(event.target.action, {
+//     method: form.method,
+//     body: data,
+//     headers: {
+//         'Accept': 'application/json'
+//     }
+//   }).then(response => {
+//     if (response.ok) {
+//       hideContact();
+//       form.reset()
+//     } else {
+//       response.json().then(data => {
+//         if (Object.hasOwn(data, 'errors')) {
+//           status.innerHTML = data["errors"].map(error => error["message"]).join(", ")
+//         } else {
+//           status.innerHTML = "Oops! There was a problem submitting your form"
+//         }
+//       })
+//     }
+//   }).catch(error => {
+//     status.innerHTML = "Oops! There was a problem submitting your form"
+//   });
+// }
+// form.addEventListener("submit", handleSubmit)
 
-async function handleSubmit(event) {
-  event.preventDefault();
-  var status = document.getElementById("contact_status");
-  var data = new FormData(event.target);
-  fetch(event.target.action, {
-    method: form.method,
-    body: data,
-    headers: {
-        'Accept': 'application/json'
-    }
-  }).then(response => {
-    if (response.ok) {
-      hideContact();
-      form.reset()
-    } else {
-      response.json().then(data => {
-        if (Object.hasOwn(data, 'errors')) {
-          status.innerHTML = data["errors"].map(error => error["message"]).join(", ")
-        } else {
-          status.innerHTML = "Oops! There was a problem submitting your form"
-        }
-      })
-    }
-  }).catch(error => {
-    status.innerHTML = "Oops! There was a problem submitting your form"
-  });
-}
-form.addEventListener("submit", handleSubmit)
-
-function sendForm(){
-  event.preventDefault();
-  $.ajax({
-    data:{
-      name: $(contact_form).serializeArray()[0].value,
-      subject: $(contact_form).serializeArray()[1].value,
-      email: $(contact_form).serializeArray()[2].value,
-      message: $(contact_form).serializeArray()[3].value
-    },
-    type: "POST",
-    url: contact_form.action,
-  });
-  hideContact();
-  return false;
-}
+// function sendForm(){
+//   event.preventDefault();
+//   $.ajax({
+//     data:{
+//       name: $(contact_form).serializeArray()[0].value,
+//       subject: $(contact_form).serializeArray()[1].value,
+//       email: $(contact_form).serializeArray()[2].value,
+//       message: $(contact_form).serializeArray()[3].value
+//     },
+//     type: "POST",
+//     url: contact_form.action,
+//   });
+//   hideContact();
+//   return false;
+// }
 
 $('.project_div').hover(
   function() {
@@ -188,15 +188,15 @@ function selectTab(id){
 
 }
 
-function projectSlide(prev=false) {
-  $(projects[current_project]).removeClass('selected')
-  if (prev) {
-    current_project = (current_project + projects.length - 1) % projects.length;
-  } else {
-    current_project = (current_project + 1) % projects.length;
-  }
-  $(projects[current_project]).addClass('selected')
-}
+// function projectSlide(prev=false) {
+//   $(projects[current_project]).removeClass('selected')
+//   if (prev) {
+//     current_project = (current_project + projects.length - 1) % projects.length;
+//   } else {
+//     current_project = (current_project + 1) % projects.length;
+//   }
+//   $(projects[current_project]).addClass('selected')
+// }
 
 $('.navbar-collapse .nav-link').on('click', function(){
     $('.navbar-collapse').collapse('hide');
@@ -205,14 +205,17 @@ function setURL(){
 
 }
 
-window.onscroll = function() {scrollNavbar()};
+// window.onscroll = function() {
+//   if
+//   scrollNavbar()
+// };
 
 function main() {
-  typeHello();
+  // typeHello();
   getSectionsOffsets();
-  if (loc != 'hello') {
-    goToSection(loc);
-  }
+  // if (loc != 'hello') {
+  //   goToSection(loc);
+  // }
   setTimeout(function(){
     $('#hello_arrow').css('animation-name', 'blink')}, 2000);
 }
